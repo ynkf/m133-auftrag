ac799ee00b0998915260489d2ec6d805b91e4411
Yannick Frischknecht <yannickf@bluewin.ch>
2019-11-27 14:57:36 +0100

    base

---
+++ app/models/queue.model.ts
@@ -0,0 +1,17 @@
+export class Queue {
+    size(): number {
+        throw new Error("Method not implemented.");
+    }
+
+    enqueue(value) {
+        throw new Error("Method not implemented.");
+    }
+
+    peek() {
+        throw new Error("Method not implemented.");
+    }
+
+    isEmpty(): boolean {
+        throw new Error("Method not implemented.");
+    }
+}

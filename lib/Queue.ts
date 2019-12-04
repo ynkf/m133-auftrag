31d9fd8642145d9cfa4daf2e15ab0b0f6736d3cf
Yannick Frischknecht <yannickf@bluewin.ch>
2019-12-04 14:51:55 +0100

    implemented all functions

--- lib/Queue.ts
+++ lib/Queue.ts
@@ -6,4 +6,20 @@ export class Queue implements IDataScructure {
     public size(): number {
         return this.queue.length;
     }
+
+    public enqueue(value: any) {
+        this.queue.push(value);
+    }
+
+    public isEmpty(): boolean {
+        return this.queue.length < 1;
+    }
+
+    public peek(): any {
+        return this.queue[0];
+    }
+
+    public poll(): any {
+        return this.queue.shift();
+    }
 }
--- test/structures.tests.ts
+++ test/structures.tests.ts
@@ -7,6 +7,46 @@ describe("Data structures", () => {
             const testee = new Queue();
             const result = testee.size();
             expect(result).to.equal(0);
+        });
+
+        it("can add", () => {
+            const queue = new Queue();
+            queue.enqueue("word");
+            const result = queue.size();
+            expect(result).to.be.equal(1);
+        });
+
+        it("queue is empty", () => {
+            const queue = new Queue();
+            const result = queue.isEmpty();
+            expect(result).to.be.equal(true);
+        });
+
+        it("queue is not empty", () => {
+            const queue = new Queue();
+            queue.enqueue("word");
+            const result = queue.isEmpty();
+            expect(result).to.be.equal(false);
+        });
+
+        it("peek next item", () => {
+            const item = "word";
+            const queue = new Queue();
+            queue.enqueue(item);
+            const result = queue.peek();
+            expect(result).to.be.equal(item);
+        });
+
+        it("poll next item", () => {
+            const item1 = "item1";
+            const item2 = "item2";
+            const queue = new Queue();
+            queue.enqueue(item1);
+            queue.enqueue(item2);
+            const removed = queue.poll();
+            const remaining = queue.peek();
+            expect(removed).to.be.equal(item1);
+            expect(remaining).to.be.equal(item2);
         })
     })
 
